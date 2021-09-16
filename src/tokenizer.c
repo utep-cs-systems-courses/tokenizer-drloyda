dd2412220318128393ec3d353fec0daca0719329
Dustyn <dlaydo@gmail.com>
2021-09-16 14:35:42 -0600

    implemented more methods and created makefile

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,18 +1,69 @@
-#include <tokenizer.h>
 #include <stdio.h>
+#include <string.h>
+#include "tokenizer.h"
 
 int space_char(char c)
 {
-  if(c ==" " || c == "\t"){
+  //if character is space or tab return true
+  if(c ==' ' || c == '\t'){
+    return 1;
+  }
+  return 0;
+}
+
+int non_space_char(char c)
+{
+  //if character is space,tab,or terminator return false
+  if(c == ' ' || c == '\t' || c == '\0'){
     return 0;
   }
   return 1;
 }
 
-int non_space_char(char c)
+char *word_start(char *str)
 {
-  if(c == " " || c == "\t" || c == "\0"){
-    return 1;
+  int i = 0;
+  while(non_space_char(str[i]) ==0){
+    i++;
+  }
+  return &str[i];
+}
+
+char *word_terminator(char *str)
+{
+  int i = 0;
+  while(space_char(str[i]) == 0){
+    i++;
+  }
+  //printf("%s\n", &str[i]);
+  return &str[i];
+}
+
+int count_words(char *str)
+{
+  int count = 0;
+  int i = 0;
+  while(1){
+    //moves to start of the word
+    word_start(&str[i]);
+    //if at end of word, count it
+    if(space_char(str[i]) && str[i+2] != '\0'){
+      count++;
+    }
+
+    //if at end of sentence
+    if(str[i] == '\0' && count != 0){
+      count++;
+      // printf("%d\n", count);
+      return count;
+    }
+    
+    //if sentence is empty
+    else if (str[i] == '\0' && count == 0){
+      //printf("%d\n", count);
+      return count;
+    }
+    i++;
   }
   return 0;
 }
--- src/uimain.c
+++ src/uimain.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "tokenizer.h"
 
 int main()
 {
   char userIn[1001];
-  while(1){
-    puts("$");
-    fgets(userIn, 100, stdin);
-    printf("%s\n",userIn);
-  }
+  // while(1){
+  puts("$");
+  fgets(userIn, 100, stdin);
+  printf("%s\n", userIn);
+  word_start(userIn);
+    // }
+ 
   return 0;
 }
